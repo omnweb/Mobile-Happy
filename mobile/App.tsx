6a6e9127dc6bd06c21a3e3c42a8a910f1fe8c230
netomatiazi <netoomn@gmail.com>
2020-10-15 18:08:33 -0300

    Creating the details view on and adding navigation elements between components

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,11 +1,8 @@
 import React from 'react';
-import { StyleSheet, Text, View, Dimensions, TouchableOpacity } from 'react-native'; // Dimensions retorna a dimensão da tela
-import MapView, { Callout, Marker, PROVIDER_GOOGLE } from 'react-native-maps' // Importando mapa
 
 import { useFonts } from 'expo-font'
 import { Nunito_600SemiBold, Nunito_700Bold, Nunito_800ExtraBold } from '@expo-google-fonts/nunito'
-import { Feather } from '@expo/vector-icons'
-import mapMarker from './src/images/map-marker.png'
+import Routes from './src/routes'
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -17,96 +14,8 @@ export default function App() {
     return null
   }
   return (
-    <View style={styles.container}>
-      <MapView
-        style={styles.map}
-        provider={PROVIDER_GOOGLE}
-        initialRegion={{
-          latitude: -22.501896,
-          longitude: -48.5567987,
-          latitudeDelta: 0.008,
-          longitudeDelta: 0.008,
-        }} >
-
-        <Marker
-          icon={mapMarker}
-          calloutAnchor={{
-            x: 2.7,
-            y: 1.1,
-          }}
-          coordinate={{
-            latitude: -22.501896,
-            longitude: -48.5567987,
-          }}
-        >
-          <Callout tooltip={true} onPress={() => { }}>
-            <View style={styles.calloutContainer}>
-              <Text style={styles.calloutText}>Lar das Meninas</Text>
-            </View>
-            <Text></Text>
-          </Callout>
-        </Marker>
-      </MapView>
-      <View style={styles.footer}>
-        <Text style={styles.footerText}> 2 Orfanatos encontrados</Text>
-        <TouchableOpacity style={styles.createOrphanageButton} onPress={() => { }}>
-          <Feather name="plus" size={20} color="#fff" />
-        </TouchableOpacity>
-      </View>
-    </View>
-  );
+    <Routes />
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  map: {
-    width: Dimensions.get('window').width,
-    height: Dimensions.get('window').height
-  },
-  calloutContainer: {
-    fontFamily: 'Nunito_700Bold',
-    width: 160,
-    height: 46,
-    paddingHorizontal: 16,
-    backgroundColor: 'rgba(255, 255, 255, 0.8)',
-    borderRadius: 16,
-    justifyContent: 'center'
-  },
-  calloutText: {
-    color: '#0089a5',
-    fontSize: 14,
 
-  },
-  footer: {
-    position: 'absolute',
-    left: 24,
-    right: 24,
-    bottom: 32,
-
-    backgroundColor: '#fff',
-    borderRadius: 20,
-    height: 56,
-    paddingLeft: 24,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-
-    elevation: 3
-  },
-  footerText: {
-    fontFamily: 'Nunito_700Bold',
-    color: '#8fa7b3',
-  },
-  createOrphanageButton: {
-    width: 56,
-    height: 56,
-    backgroundColor: '#15c3d6',
-    borderRadius: 17,
-
-    justifyContent: 'center',
-    alignItems: 'center'
-
-  },
-});
---
+++ mobile/src/pages/OrphanagesMap.tsx
@@ -0,0 +1,105 @@
+import React from 'react'
+import { StyleSheet, Text, View, Dimensions, TouchableOpacity } from 'react-native'; // Dimensions retorna a dimensão da tela
+import MapView, { Callout, Marker, PROVIDER_GOOGLE } from 'react-native-maps' // Importando mapa
+import { Feather } from '@expo/vector-icons'
+import mapMarker from '../images/map-marker.png'
+import { useNavigation } from '@react-navigation/native';
+
+export default function OrphanagesMap() {
+    const navigation = useNavigation()
+    function handleNavigateToOrphanageDetails() {
+        navigation.navigate('OrphanageDetails')
+    }
+    return (
+        <View style={styles.container}>
+            <MapView
+                style={styles.map}
+                provider={PROVIDER_GOOGLE}
+                initialRegion={{
+                    latitude: -22.501896,
+                    longitude: -48.5567987,
+                    latitudeDelta: 0.008,
+                    longitudeDelta: 0.008,
+                }} >
+
+                <Marker
+                    icon={mapMarker}
+                    calloutAnchor={{
+                        x: 2.7,
+                        y: 1.1,
+                    }}
+                    coordinate={{
+                        latitude: -22.501896,
+                        longitude: -48.5567987,
+                    }}
+                >
+                    <Callout tooltip={true} onPress={handleNavigateToOrphanageDetails}>
+                        <View style={styles.calloutContainer}>
+                            <Text style={styles.calloutText}>Lar das Meninas</Text>
+                        </View>
+                        <Text></Text>
+                    </Callout>
+                </Marker>
+            </MapView>
+            <View style={styles.footer}>
+                <Text style={styles.footerText}> 2 Orfanatos encontrados</Text>
+                <TouchableOpacity style={styles.createOrphanageButton} onPress={() => { }}>
+                    <Feather name="plus" size={20} color="#fff" />
+                </TouchableOpacity>
+            </View>
+        </View>
+    )
+}
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    map: {
+        width: Dimensions.get('window').width,
+        height: Dimensions.get('window').height
+    },
+    calloutContainer: {
+        fontFamily: 'Nunito_700Bold',
+        width: 160,
+        height: 46,
+        paddingHorizontal: 16,
+        backgroundColor: 'rgba(255, 255, 255, 0.8)',
+        borderRadius: 16,
+        justifyContent: 'center'
+    },
+    calloutText: {
+        color: '#0089a5',
+        fontSize: 14,
+
+    },
+    footer: {
+        position: 'absolute',
+        left: 24,
+        right: 24,
+        bottom: 32,
+
+        backgroundColor: '#fff',
+        borderRadius: 20,
+        height: 56,
+        paddingLeft: 24,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+
+        elevation: 3
+    },
+    footerText: {
+        fontFamily: 'Nunito_700Bold',
+        color: '#8fa7b3',
+    },
+    createOrphanageButton: {
+        width: 56,
+        height: 56,
+        backgroundColor: '#15c3d6',
+        borderRadius: 17,
+
+        justifyContent: 'center',
+        alignItems: 'center'
+
+    },
+});
